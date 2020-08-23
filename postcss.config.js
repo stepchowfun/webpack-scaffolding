84a9384b3d39bc99e77c2044eea52b35b68cceff
Stephan Boyer <stephan@stephanboyer.com>
2020-08-23 06:36:49 -0700

    Use `styled-components`

--- postcss.config.js
+++ postcss.config.js
@@ -1,5 +1,11 @@
 const autoprefixer = require('autoprefixer');
+const cssnano = require('cssnano');
 
 module.exports = {
-  plugins: [autoprefixer],
+  plugins: [
+    autoprefixer,
+    cssnano({
+      preset: 'default',
+    }),
+  ],
 };
--- src/app/app.tsx
+++ src/app/app.tsx
@@ -1,16 +1,21 @@
 import React from 'react';
+import styled from 'styled-components';
 import smiley from './smiley.svg';
-import './app.scss';
 import sum from '../sum/sum';
+
+const AppContainer = styled.div`
+  color: #333333;
+  text-align: center;
+`;
 
 function App() {
   return (
-    <div className="app">
+    <AppContainer>
       <p>
         <img src={smiley} alt="Smiley face" />
       </p>
       <p>1 + 2 = {sum(1, 2)}</p>
-    </div>
+    </AppContainer>
   );
 }
 
